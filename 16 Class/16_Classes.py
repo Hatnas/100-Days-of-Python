99e425ed5d417baab4f993762167e7fd652f9c1e
Hatnas <leonardo.mastrangelo@gmail.com>
2025-04-28 11:20:04 -0300

    Terminado el video 5 del dia 16 PrettyTable

--- 16 Class/16_Classes.py
+++ 16 Class/16_Classes.py
@@ -4,10 +4,20 @@
 # y, por otro lado tiene "Metodos" Que son funciones que determinan que hace
 
 
-# Pcrear un objeto que sea de una clase tenemos que primero establecer la clase
+
+#La data de PrettyTable esta en https://pypi.org/project/prettytable/
+# Para crear un objeto que sea de una clase tenemos que primero establecer la clase
 #eso lo hacemos importando la clase "PrittyTable"
 
 from prettytable import PrettyTable
 
 #Ya teniendo la clase creo una "variable" que la vinculo con la clase
 table = PrettyTable()
+print (table)
+
+table.add_column("Pokemon Name",["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+table.align = "l"
+print (table)
+
