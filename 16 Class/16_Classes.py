3e590c916db4257c00425ea2f0f0637f860fc811
Hatnas <leonardo.mastrangelo@gmail.com>
2025-04-28 10:13:45 -0300

    Se crea el objeto table de la clase PrettyTable

--- 16 Class/16_Classes.py
+++ 16 Class/16_Classes.py
@@ -1,5 +1,13 @@
 ### Classes ###
 
 # Una clase tiene "Atributos" Que son variables que las caracterizan
-# y, por otro lado tiene "Metodos" Que son funciones que determinan que hace#
+# y, por otro lado tiene "Metodos" Que son funciones que determinan que hace
 
+
+# Pcrear un objeto que sea de una clase tenemos que primero establecer la clase
+#eso lo hacemos importando la clase "PrittyTable"
+
+from prettytable import PrettyTable
+
+#Ya teniendo la clase creo una "variable" que la vinculo con la clase
+table = PrettyTable()
