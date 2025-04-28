ac98be35228a09abf5c72f5c294154de624b4df5
Hatnas <leonardo.mastrangelo@gmail.com>
2025-04-28 10:00:31 -0300

    Agregada la Tortuga y la PrettyTable desde pip

--- 100_Days_Pruebas.py
+++ 100_Days_Pruebas.py
@@ -1,6 +1,7 @@
 var_a = "Hola"
 
 def modificacion():
+    global var_a
     if input("Chau?").lower() == "si":
         var_a = "Chau"
 
---
+++ 16 Class/16_Classes.py
@@ -0,0 +1,5 @@
+### Classes ###
+
+# Una clase tiene "Atributos" Que son variables que las caracterizan
+# y, por otro lado tiene "Metodos" Que son funciones que determinan que hace#
+
