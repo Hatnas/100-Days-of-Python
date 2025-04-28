eaeb5b0d9ee569dbfcb5b1789347bd6a70de2213
Hatnas <leonardo.mastrangelo@gmail.com>
2025-04-28 14:17:30 -0300

    Coffee Machine OOP Finish

--- 16 Class/16_Classes.py
+++ 16 Class/16_Classes.py
@@ -3,8 +3,6 @@
 # Una clase tiene "Atributos" Que son variables que las caracterizan
 # y, por otro lado tiene "Metodos" Que son funciones que determinan que hace
 
-
-
 #La data de PrettyTable esta en https://pypi.org/project/prettytable/
 # Para crear un objeto que sea de una clase tenemos que primero establecer la clase
 #eso lo hacemos importando la clase "PrittyTable"
@@ -20,4 +18,3 @@ table.add_column("Type", ["Electric", "Water", "Fire"])
 
 table.align = "l"
 print (table)
-
--- 16 Class/16_Coffee Machine.py
+++ 16 Class/16_Coffee Machine.py
@@ -1,1 +1,30 @@
-### Coffe Machine oop Version ###
+### Coffee Machine oop Version ###
+
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffe_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+is_on = True
+
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like ({options})? ")  # aca genero el input como "choice"
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffe_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice) # Aca paso el input como un MenuItem dentro de Menu
+        if coffe_maker.is_resource_sufficient(drink):
+            print (f"Its ${drink.cost}")
+
+            if money_machine.make_payment(drink.cost):
+                coffe_maker.make_coffee(drink)
+
+
+
