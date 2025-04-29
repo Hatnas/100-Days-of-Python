22a029cb75f3cc235eed67426827709bb148541a
Hatnas <leonardo.mastrangelo@gmail.com>
2025-04-29 11:08:42 -0300

    Quiz Project start, Question Class

--- 100_Days_Pruebas.py
+++ 100_Days_Pruebas.py
@@ -1,9 +1,43 @@
-var_a = "Hola"
+import random
 
-def modificacion():
-    global var_a
-    if input("Chau?").lower() == "si":
-        var_a = "Chau"
+races_list = [
+    {"raza": "Humano" ,
+    "HP": 2},
+    {"raza": "Elfo",
+     "HP": 1},
+    {"raza": "Enano",
+     "HP": 3}
+    ]
 
-modificacion()
-print (var_a)
+profession_list = [
+    {"clase": "Picaro" ,
+    "HP": 2},
+    {"clase": "Mago",
+     "HP": 1},
+    {"clase": "Guerrero",
+     "HP": 3}
+    ]
+
+
+class Character:
+    def __init__(self,):
+        self.race = "bla"
+        self.hp = 0
+        self.profession = "bla"
+
+    def stats(self):
+        random_race = random.choice(races_list)
+        self.race = random_race["raza"]
+        self.hp += int(random_race["HP"])
+
+        random_profession = random.choice(profession_list)
+        self.profession = random_profession["clase"]
+        self.hp += int(random_profession["HP"])
+
+
+name = Character()
+name.stats()
+
+print (name.race)
+print (name.profession)
+print (name.hp)
--- 17 Class creation/17_class_creation.py
+++ 17 Class creation/17_class_creation.py
@@ -8,12 +8,23 @@ class User:
     self.user_id = user_id
     self.name = user_name
     self.followers = 0   # Puedo establecer un parametro por fuera de lso que estan en el constructor.
+    self.following = 0
+
+  def follow (self,user):    # Al poner self entre parentesis en la funcion, modifico los atributos de la clase
+      user.followers += 1
+      self.following += 1
 
 # Al crear un objeto tengo que completar los parametros de los atributos
 # user_1 = User (1, "Leo")
 user_1 = User("001", "Leo")
+user_2 = User("002", "Lau")
 
 print (user_1.user_id)  # 001
 print(user_1.name) # Leo
 print(user_1.followers) # 0
 
+user_1.follow(user_2)    # al seguir user_1 a u_2 se modificaron los atributos de los dos
+print (user_1.followers)
+print (user_1.following)
+print (user_2.followers)
+print (user_2.following)
