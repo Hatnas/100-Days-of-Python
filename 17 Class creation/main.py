62dff446587bd6bccb411bc0f020a51a1143402d
Hatnas <leonardo.mastrangelo@gmail.com>
2025-04-29 15:31:13 -0300

    D17 Quiz Final

--- 17 Class creation/main.py
+++ 17 Class creation/main.py
@@ -1,6 +1,7 @@
 
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -10,4 +11,10 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-print (question_bank[0].text)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+print ("You completed the quiz!")
+print(f"Your final score is: {quiz.score}/{quiz.question_number}")
