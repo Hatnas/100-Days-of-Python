8dcde4d71062d9ec6793820ac23342f864446744
Hatnas <leonardo.mastrangelo@gmail.com>
2025-05-07 10:07:37 -0300

    Add files via upload

---
+++ dots.py
@@ -0,0 +1,59 @@
+import turtle
+
+import random
+from turtle import Turtle, Screen
+
+
+tim = Turtle()
+tim.shape("turtle")
+tim.color("sea green")
+turtle.colormode (255)
+
+# Requisitos
+# 10 x 10 dots #
+#de 20 con espacio de 50 #
+# random color #
+# siempre de izquierda a Derecha #
+
+dots_color_list = [(202, 164, 110), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20)]
+
+def random_color():
+    color = random.choice(dots_color_list)
+    return color
+
+def dot(color):
+    """ Esta funcion pone un Dot"""
+    tim.pendown()
+    tim.dot(20, color)
+
+def fd():
+    """ Mueve a la tortuga hacia adelante sin pintar"""
+    tim.penup()
+    tim.fd(50)
+
+def pattern():
+    """ Hace una linea de 10 Dots de color random """
+    for _ in range(10):
+        dot(random_color())
+        fd()
+
+
+# Aca arranca el programa en si
+y = -250
+for _ in range (10):
+    tim.teleport(-300, y)
+    pattern()
+    y += 30
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.screensize(500, 500)
+screen.exitonclick()
